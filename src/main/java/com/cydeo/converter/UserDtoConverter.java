fd7cccbadddc8c3cf81825a8fe14594c2ec813c8
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-13 23:13:02 -0400

    Project view in progress

---
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -0,0 +1,23 @@
+package com.cydeo.converter;
+
+import com.cydeo.DTO.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+//@ConfigurationPropertiesBinding - done automatically by latest version of Spring
+public class UserDtoConverter implements Converter<String, UserDTO> {
+
+    private final UserService userService;
+
+    public UserDtoConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        return userService.findById(source);
+    }
+}
