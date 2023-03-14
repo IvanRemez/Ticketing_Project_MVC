55453e046e1e48d6fbdfd001de531012328e065c
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-14 13:26:18 -0400

    User view done

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -33,6 +30,33 @@ public class UserController {
     public String insertUser(@ModelAttribute("user") UserDTO user) {
 
         userService.save(user);
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model) {
+    // user object ${user}
+        model.addAttribute("user", userService.findById(username));
+
+        model.addAttribute("listRoles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+        return "/user/update";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") UserDTO user) {
+
+        userService.update(user);
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username) {
+
+        userService.deleteById(username);
 
         return "redirect:/user/create";
     }
