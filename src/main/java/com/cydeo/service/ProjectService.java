39bffc35c5dbc6126b2862341af4547d9fc9162a
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-15 22:35:57 -0400

    Project view done

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -4,4 +4,6 @@ import com.cydeo.DTO.ProjectDTO;
 
 public interface ProjectService extends CrudService<ProjectDTO, String> {
 
+    void complete(ProjectDTO project);
+
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -2,6 +2,10 @@ package com.cydeo.service;
 
 import com.cydeo.DTO.UserDTO;
 
+import java.util.List;
+
 public interface UserService extends CrudService<UserDTO, String> {
+
+    List<UserDTO> findManagers();
 
 }
